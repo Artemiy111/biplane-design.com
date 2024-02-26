10032601ac0ee8a12698127b86d224be91e81e2e
Artemiy111 <mrart111@mail.ru>
2024-02-26 21:47:26 +0300

    chore: file detection

---
+++ server/listen.ts
@@ -0,0 +1,97 @@
+/* eslint-disable no-console */
+import fs from 'node:fs'
+import path from 'node:path'
+import { cwd } from 'node:process'
+import chokidar from 'chokidar'
+
+import { and, eq } from 'drizzle-orm'
+import { images, projects } from './db/schema'
+import { db } from '~/server/db'
+
+const dir = `${cwd()}/public/images/projects`
+
+function extractImageInfo(path: string) {
+  const params = path.split('/')
+  if (params.length !== 2)
+    return null
+  const projectUrlFriendly = params[0]
+  const filename = params[1]
+  const fileArgs = filename.split('.')
+  if (fileArgs.length !== 2)
+    return null
+
+  return {
+    projectUrlFriendly,
+    filename,
+  }
+}
+
+chokidar.watch(dir, { cwd: dir, ignoreInitial: false, atomic: true, depth: 1 })
+  .on('add', async (path) => {
+    const info = extractImageInfo(path)
+    if (!info)
+      return
+
+    try {
+      const project = await db.select().from(projects).where(eq(projects.urlFriendly, info.projectUrlFriendly))
+      if (!project.length) {
+        console.log(`${info.projectUrlFriendly} / не существует в БД. Пропускаю добавление изображения к БД`)
+        return
+      }
+      const image = await db.select().from(images).where(and(
+        eq(images.projectUrlFriendly, info.projectUrlFriendly),
+        eq(images.filename, info.filename),
+      ))
+      if (image.length) {
+        console.log(`${info.projectUrlFriendly} / ${info.filename} =`)
+        return
+      }
+
+      await db.insert(images).values({ projectUrlFriendly: info.projectUrlFriendly, filename: info.filename })
+      console.log(`${info.projectUrlFriendly} / ${info.filename} +`)
+    }
+    catch (e) {
+      console.error(`Не удалось добавить изображение ${info.filename} к проекту ${info.projectUrlFriendly}`, e)
+    }
+  })
+  .on('unlink', async (path) => {
+    const info = extractImageInfo(path)
+    if (!info)
+      return
+
+    try {
+      const project = await db.select().from(projects).where(eq(projects.urlFriendly, info.projectUrlFriendly))
+      if (!project.length) {
+        console.log(`${info.projectUrlFriendly} / не существует в БД. Пропускаю удаление изображения из БД`)
+        return
+      }
+
+      const deletedImages = await db.delete(images).where(and(
+        eq(images.projectUrlFriendly, info.projectUrlFriendly),
+        eq(images.filename, info.filename),
+      )).returning()
+
+      if (deletedImages.length)
+        console.log(`${info.projectUrlFriendly} / ${info.filename} -`)
+    }
+    catch (e) {
+      console.error(`Не удалось удалить изображение ${info.filename} из проекта ${info.projectUrlFriendly}`, e)
+    }
+  })
+  .on('unlinkDir', async (projectUrlFriendly) => {
+    try {
+      const project = await db.select().from(projects).where(eq(projects.urlFriendly, projectUrlFriendly))
+      if (!project.length) {
+        console.log(`${projectUrlFriendly} / не существует. Пропускаю удаление изображений из БД`)
+        return
+      }
+
+      const deletedImages = await db.delete(images).where(eq(images.projectUrlFriendly, projectUrlFriendly)).returning()
+      deletedImages.forEach(image =>
+        console.log(`${projectUrlFriendly} / ${image.filename} -`),
+      )
+    }
+    catch (e) {
+      console.error(`Не удалось удалить проект ${projectUrlFriendly} из БД`)
+    }
+  })
---
+++ server/scan.ts
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+import fs from 'node:fs'
+import path from 'node:path'
+import { cwd } from 'node:process'
+import { and, eq } from 'drizzle-orm'
+import type { Image } from './db/schema'
+import { images, projects } from './db/schema'
+import { client, db } from '~/server/db'
+
+const dbProjects = await db.query.projects.findMany({ with: { images: true } })
+
+const dir = `${cwd()}/public/images/projects`
+
+function findNonExistingImages(dbImages: Image[]): Image[] {
+
+}
+
+// Проект есть в БД но нет в папке      -> информируем
+// Проекта нет в БД, но есть в папке    -> уведомляем
+// Проект есть в БД
+// - фото есть в БД, но нет в папке     -> удаляем из БД
+// - фото есть в папке, но нет в БД     -> добавляем в БД
+
+dbProjects.forEach(async (project) => {
+  try {
+    const paths = (fs.readdirSync(`${dir}/${project.urlFriendly}`))
+    const files = paths.filter(p => fs.statSync(path.join(dir, project.urlFriendly, p)).isFile())
+
+    const imagesFilenames = files.filter((f) => {
+      const ext = f.split('.')[1]
+      if (!ext)
+        return false
+      if (ext === 'avif' || ext === 'webp' || ext === 'png' || ext === 'jpg' || ext === 'jpg')
+        return true
+      return false
+    })
+
+    project.images.forEach((image) => {
+      if (!imagesFilenames.find(img => img === image.filename)) {
+        console.log(`${project.urlFriendly} / ${image.filename} Не существует в директории. Удаляю из БД`)
+        db.delete(images).where(and(
+          eq(images.projectUrlFriendly, image.projectUrlFriendly),
+          eq(images.filename, image.filename),
+        )).catch(_e => console.log(`${project.urlFriendly} / ${image.filename} Не удалось удалить из БД`))
+      }
+    })
+  }
+  catch (e) {
+    console.error(`${project.urlFriendly} / Не существует`)
+  }
+})
+
+client.end()
