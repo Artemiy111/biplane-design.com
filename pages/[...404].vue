1df9fbb7536404ef1ac02448ebb117959a62acd7
Artemiy111 <mrart111@mail.ru>
2024-03-08 20:48:31 +0300

    chore: add page 404

---
+++ pages/[...404].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { buttonVariants } from '~/components/ui/button'
+</script>
+
+<template>
+  <main class="container flex flex-col items-center justify-center gap-8 pt-[20dvh]">
+    <div class="flex flex-col items-center gap-1">
+      <span class="text-[140px] leading-none lg:text-[120px] md:text-[100px]">404</span>
+      <span>Страница не найдена</span>
+    </div>  <NuxtLink to="/projects" :class="buttonVariants()">
+      Смотреть проекты
+    </NuxtLink>
+  </main>
+</template>
