676edc4cc412f09734259ae354a1e47bd3b3e917
Artemiy111 <mrart111@mail.ru>
2024-06-13 15:15:10 +0300

    chore: bun

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -3,14 +3,15 @@ import { Argon2id } from 'oslo/password'
 import { userRepo } from '~/server/di'
 import { lucia } from '~/utils/auth'
 
-const registerScema = z.object({
+const registerSchema = z.object({
   username: z.string().min(3),
   password: z.string().min(6),
 
 })
 
 export default defineEventHandler(async (event) => {
-  const data = await readValidatedBody(event, registerScema.parse)
+  throw createError({ statusCode: 500, message: 'Нельзя зарегистрировать нового администратора' })
+  const data = await readValidatedBody(event, registerSchema.parse)
 
   const passwordHash = await new Argon2id().hash(data.password)
   const createdUser = await userRepo.create({
