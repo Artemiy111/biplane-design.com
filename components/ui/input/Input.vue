4a70663470e44c7ab487d53185a23a171eed0a36
Artemiy111 <mrart111@mail.ru>
2024-10-07 17:07:14 +0300

    lint

--- components/ui/input/Input.vue
+++ components/ui/input/Input.vue
@@ -25,5 +25,10 @@ defineExpose({
 </script>
 
 <template>
-  <input ref="inputRef" v-model="modelValue" v-bind="$attrs" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', props.class)">
+  <input
+    ref="inputRef"
+    v-model="modelValue"
+    v-bind="$attrs"
+    :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', props.class)"
+  >
 </template>
