650928db6e3399f83cbe55175aaa22ec4c6061ef
Artemiy111 <mrart111@mail.ru>
2025-04-18 16:37:33 +0300

    try to fix build

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
-import { useGroupsQuery, useUserQuery } from '~~/src/shared/model/queries'
 import { Footer } from '~~/src/widgets/footer'
 import { Header } from '~~/src/widgets/header'
 
-// useGroups()
-// useUser()
 </script>
 
 <template>
   <div class="grid min-h-dvh grid-rows-[auto_1fr_auto] text-base font-normal text-foreground">
-    <!-- <div class="flex size-full min-h-dvh flex-col"> -->
     <Header />
     <NuxtPage />
-    <!-- </div> -->
     <Footer />
   </div>
 </template>
